a8262dd7444fee8f597d8087ecbddd94db6b9a5c
Jeppe <jepp026b@stud.kea.dk>
2024-06-14 15:39:09 +0200

    testing git

--- src/functions/postJson.ts
+++ src/functions/postJson.ts
@@ -11,6 +11,6 @@ export async function postJson(data: unknown): Promise<{ status: number}> {
     return { status: response.status};
   } catch (error) {
     console.error('Error adding grid item:', error);
-    throw error; // throw to caller
+    throw error; // throw to caller s
   }
 }
