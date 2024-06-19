6c2aeab3db85b8ce7ec435dc0910e1b2e3101078
Jeppe <jepp026b@stud.kea.dk>
2024-06-20 01:35:56 +0200

    Pagination

--- src/hooks/usePriceQuery.ts
+++ src/hooks/usePriceQuery.ts
@@ -4,9 +4,10 @@ import { PriceResponse } from "../models/PriceResponse";
 import priceStore from "../stores/priceStore";
 import { useState } from "react";
 
-const usePriceQuery = (coinId: number, paginate: boolean = false) => {
-  const { page } = priceStore();
-  const [currentPage, setCurrentPage] = useState(page);
+const usePriceQuery = (coinId: number, paginate: boolean = false, page: number = 1) => {
+  // const { page } = priceStore();
+
+
 
   const fetchPrices = async (page?: number): Promise<PriceResponse> => {
     console.log("COINID", coinId);
@@ -16,16 +17,14 @@ const usePriceQuery = (coinId: number, paginate: boolean = false) => {
     return priceData;
   };
 
-  const queryKey = paginate ? ["coin", coinId, "prices"] : ["coin", coinId, "prices", currentPage]
+  const queryKey = paginate ? ["coin", coinId, "prices", page] : ["coin", coinId, "prices"]
 
   return {
     ...useQuery<PriceResponse, Error>({
       queryKey,
-      queryFn: () => fetchPrices(currentPage),
+      queryFn: () => fetchPrices(page),
       placeholderData: keepPreviousData,// Retains the previous data while fetching the new page
     }),
-    currentPage,
-    setCurrentPage,
   };
 };
 
